5b7ae0d7ad6973feb43ec4278d1d82c8d4596bb0
Karlacruz11 <cruz.pekarla@gmail.com>
2022-07-21 12:12:48 -0500

    Version Prueba

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import validator from './validator.js';
 
-console.log(validator);
+  let submitValidator = document.getElementById("submitval"); // Encuentra el elemento "submitval" en el sitio
+  submitValidator.onclick = function() {validator.isValid()};
+
+
+document.getElementById("CardNumber").addEventListener("blur", numerotarjeta);
+    function numerotarjeta (event) {
+      let Numcard = event.target.value;
+
+      event.target.value = validator.maskify(Numcard);
+    }
+
+    //validacion de input text solo acepta numeros
+let solonumeros = document.getElementById("CardNumber");
+solonumeros.addEventListener("keyup", (e)=>{
+  let valorinput = e.target.value; //guardando el valor del input
+   solonumeros.value = valorinput
+    //eliminar espacios en blanco
+   .replace(/\D/g, '')//eliminar las letras
+   .replace(/([0-9]{4})/g, '$1 ') //espacio cada cuatro numeros
+   .trim();
+});
+
+
+
+
+  //console.log(isValid)
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,76 @@
-const validator = {
-  // ...
-};
+let numnormal ;
+const validator=  {
+//function isValid() {
+  //logica de isvalid
+//},
+
+isValid:function(){
+  let CCnum = numnormal.replace(/\s/g, '') ;
+  let CCnumSplit = CCnum.split("");
+  let sum = 0;
+  let imparNum = [], parNum = [] ;
+
+  for(let i=0; CCnum.length>i; i++){
+ //validar par
+   if(i % 2==0){
+      let mulpar= "" + parseInt((CCnumSplit[i]) * 2);
+   if(mulpar>=10){
+      let separar=  mulpar.split("");
+      let suma= parseInt(separar[0])+parseInt(separar[1]);
+      parNum.push(suma);
+      }
+      else{
+        parNum.push(mulpar);
+
+      }
+     }
+    else {
+      imparNum.push(CCnumSplit[i]);
+
+    }
+  }
+
+  console.log("PARES")
+  for (let i of parNum){
+    sum=parseInt(sum) + parseInt(i);
+    console.log("["+i+"]")
+  }
+
+  console.log("IMPARES")
+  for (let i of imparNum){
+    sum=parseInt(sum)  + parseInt(i);
+    console.log("["+i+"]")
+  }
+
+  console.log("suma total= " + sum)
+  if(sum % 10 == 0){
+    alert("es valida")
+  }
+  else{
+    alert(" invalida")
+  }
+},
+
+
+
+maskify: function (Numcard) {
+  let MaskNum = ""
+  numnormal = Numcard
+  if(Numcard.length > 4) {
+    MaskNum +=
+    "#".repeat(Numcard.length -4 ) + Numcard.slice(-4);
+  return MaskNum;
+  }
+   else {
+     return Numcard;
+   }
+
+}
+
+
+
+} ;
+
+
 
 export default validator;
