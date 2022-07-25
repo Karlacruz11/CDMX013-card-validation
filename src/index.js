87d3ee3bd4ac5f9688845995697df2a58dfd5567
Karlacruz11 <cruz.pekarla@gmail.com>
2022-07-25 10:15:06 -0500

    Prueba

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 import validator from './validator.js';
 
-  let submitValidator = document.getElementById("submitval"); // Encuentra el elemento "submitval" en el sitio
-  submitValidator.onclick = function() {validator.isValid()};
+document.getElementById("submitval").addEventListener("click",callvalidator );
+  function callvalidator () {
+    let creditCardNumber = document.getElementById("CardNumber").value ; // guardando el valor del input
+          creditCardNumber = creditCardNumber.replace(/\s/g, '');
+        //  console.log("primero"+creditCardNumber);
+document.getElementById("CardNumber").value = validator.maskify(creditCardNumber); //muestra en HTML el numero enmascarado
 
+      if (validator.isValid(creditCardNumber) && creditCardNumber.length > 0) {
+    document.getElementById("Message").innerHTML = "<span style= 'color: green; background: #C3E0CA;'> Tarjeta Valida </span>";
+      }
+      else {
+    document.getElementById("Message").innerHTML = "<span style= 'color: red; background: #C3E0CA;'> Tarjeta Invalida </span>" ;
+      }
+  let valueExp = document.getElementById("Cexp").value ;
+   let Cexp = valueExp.split('/') ; // divide la cadena en arreglo
+  let mes = parseInt (Cexp [0]) ; //convierte la cadena en entero
+   if (mes <= 12 && mes > 0){
+    console.log("Mes correcto")
+   }
+   else {
+     console.log( "Mes incorrecto")
+   }
+ const fecha = new Date()
+   let año = parseInt (Cexp [1]) ;
+   if (año >= fecha.getFullYear().toString().slice(-2)) {
+     console.log ("Ano correcto")
+   }
+   else {
+     console.log ("Ano invalido")
+   }
 
-document.getElementById("CardNumber").addEventListener("blur", numerotarjeta);
-    function numerotarjeta (event) {
-      let Numcard = event.target.value;
-
-      event.target.value = validator.maskify(Numcard);
+   let valueCVV = document.getElementById("CCV").value ;
+   if (/^[0-9]{3}$/.test(valueCVV)){
+      console.log(" correcto")
+   }
+    else {
+      console.log( "incorrecto")
     }
 
+
+  }
     //validacion de input text solo acepta numeros
 let solonumeros = document.getElementById("CardNumber");
 solonumeros.addEventListener("keyup", (e)=>{
   let valorinput = e.target.value; //guardando el valor del input
    solonumeros.value = valorinput
-    //eliminar espacios en blanco
    .replace(/\D/g, '')//eliminar las letras
    .replace(/([0-9]{4})/g, '$1 ') //espacio cada cuatro numeros
    .trim();
@@ -25,4 +54,6 @@ solonumeros.addEventListener("keyup", (e)=>{
 
 
 
-  //console.log(isValid)
+
+
+//  console.log(validator)
--- src/validator.js
+++ src/validator.js
@@ -1,68 +1,68 @@
-let numnormal ;
 const validator=  {
 //function isValid() {
   //logica de isvalid
 //},
 
-isValid:function(){
-  let CCnum = numnormal.replace(/\s/g, '') ;
-  let CCnumSplit = CCnum.split("");
-  let sum = 0;
-  let imparNum = [], parNum = [] ;
+isValid:function(creditCardNumber){
+  //let CCnum = numnormal.replace(/\s/g, '') ; //eliminar espacios en blanco
+  let CCnumSplit = creditCardNumber.split("");
+  let sum = 0; // inicializa la variable
+  let imparNum = [], parNum = [] ; //inicializando arreglos
 
-  for(let i=0; CCnum.length>i; i++){
+  for(let i=0; creditCardNumber.length>i; i++){   //recorre arreglos
  //validar par
    if(i % 2==0){
       let mulpar= "" + parseInt((CCnumSplit[i]) * 2);
-   if(mulpar>=10){
+    if(mulpar>=10){
       let separar=  mulpar.split("");
       let suma= parseInt(separar[0])+parseInt(separar[1]);
       parNum.push(suma);
       }
-      else{
+    else{
         parNum.push(mulpar);
-
       }
      }
-    else {
+  else {
       imparNum.push(CCnumSplit[i]);
 
     }
   }
 
-  console.log("PARES")
-  for (let i of parNum){
+ //console.log("PARES")
+  for (let i of parNum){    //toma una iteracion del arreglo
     sum=parseInt(sum) + parseInt(i);
-    console.log("["+i+"]")
+   //console.log("["+i+"]")
   }
 
-  console.log("IMPARES")
+  //console.log("IMPARES")
   for (let i of imparNum){
     sum=parseInt(sum)  + parseInt(i);
-    console.log("["+i+"]")
+  //  console.log("["+i+"]")
   }
 
-  console.log("suma total= " + sum)
-  if(sum % 10 == 0){
-    alert("es valida")
-  }
-  else{
-    alert(" invalida")
-  }
+ //console.log("suma total= " + (sum % 10 === 0))
+
+ console.log("suma total= " + (sum))
+sum = sum % 10;
+if (sum === 0) {
+  return true;
+}
+else {
+  return false;
+}
+  //operacion logica
+
 },
 
-
-
-maskify: function (Numcard) {
+maskify: function (creditCardNumber) {
   let MaskNum = ""
-  numnormal = Numcard
-  if(Numcard.length > 4) {
+  if(creditCardNumber.length > 4) {
     MaskNum +=
-    "#".repeat(Numcard.length -4 ) + Numcard.slice(-4);
+    "#".repeat(creditCardNumber.length -4 ) + creditCardNumber.slice(-4);
   return MaskNum;
   }
    else {
-     return Numcard;
+     return creditCardNumber;
    }
 
 }
